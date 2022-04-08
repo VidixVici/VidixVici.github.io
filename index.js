03350a95fdcaaca8b097be8daea979c1876a1ce7
VidixVici <thezacharyharbert@gmail.com>
2022-04-08 02:52:27 -0500

    Update: testing 3js

---
+++ index.js
@@ -0,0 +1,4 @@
+
+import { OrbitControls } from 'https://unpkg.com/three@0.139.2/examples/jsm/controls/OrbitControls.js';
+const controls = new OrbitControls( camera, renderer.domElement );
+       
