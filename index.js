058d5f5713ab6918b217c14cb767038dbe0367f3
VidixVici <thezacharyharbert@gmail.com>
2022-04-08 05:22:10 -0500

    Three.JS: Test

--- index.js
+++
@@ -1,4 +0,0 @@
-
-import { OrbitControls } from 'https://unpkg.com/three@0.139.2/examples/jsm/controls/OrbitControls.js';
-const controls = new OrbitControls( camera, renderer.domElement );
-       
